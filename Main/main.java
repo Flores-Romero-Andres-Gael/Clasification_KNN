3b7ceec653bafaddd958e681ad1fac80e104ccc3
Gael Flores <florogael@gmail.com>
2024-11-01 09:18:40 -0600

    Actualizacion del main

--- Main/main.java
+++ Main/main.java
@@ -7,7 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class main {
+public class Main {
+    
     public static void main(String[] args) {
         DataSets dataSets = new DataSets();
         dataSets.loadDataSet();
